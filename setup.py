dd3407a4a20bfe0a2200ec54473f11d9a5008447
kmvucd <vimarish.kuttapalayammani@ucdconnect.ie>
2025-05-30 17:32:31 +0100

    Created folders

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'AI-Medical-LLM',
+    version= '0.0.0',
+    author= 'Vimarish K M',
+    author_email= 'vimarish.ie@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
